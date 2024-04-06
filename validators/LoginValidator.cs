c1dc9f1d38e472ce6270776a20993604fbc123c6
fadhlaouir <raed.fadhlaoui@hotmail.com>
2024-04-06 15:13:36 +0100

    feat: Implement login validator

---
+++ validators/LoginValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using TravelAgencyBackend.Models;
+
+namespace TravelAgencyBackend.validators
+{
+    public class LoginValidator : AbstractValidator<LoginModel>
+    {
+        public LoginValidator()
+        {
+            RuleFor(login => login.Email)
+                .NotEmpty()
+                .WithMessage("Email is required")
+                .EmailAddress()
+                .WithMessage("Should be a valid email address format");
+            RuleFor(login => login.Password)
+                .NotEmpty()
+                .WithMessage("Password is required")
+                .MinimumLength(5)
+                .WithMessage("Minimum Length 5");
+        }
+    }
+}
